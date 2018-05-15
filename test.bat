bd064e34288d7d9e8a851156205c08b421dab874
Madhu Latha Chilakala <madhulatha.chilakala@cognizant.com>
2018-05-15 13:43:51 -0400

    Sample Bat push

---
+++ test.bat
@@ -0,0 +1,6 @@
+cd C:\Program Files (x86)\Jenkins\workspace\Sample\
+echo > Helloworld_Test.txt 
+"C:\Program Files\Git\bin\git.exe" add HelloWorld_Test.txt
+"C:\Program Files\Git\bin\git.exe" commit -am " Hello World.txt"
+
+exit
